2ff33689a84398c9b5ee756387848899554aa684
Johnny Pena <jpenaguitarist@gmaio.com>
2025-02-03 17:02:49 -0600

    Rename remix.config.js to remix.config.cjs to use CommonJS

---
+++ remix.config.cjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/server/index.js",
+  publicPath: "/build/",
+  server: "./server.js",
+  serverConditions: ["worker"],
+};
